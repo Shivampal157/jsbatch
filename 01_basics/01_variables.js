a5781d2b9a48d9639f43796455fa108b52940f1b
shivampal157 <shivampal60076@gmail.com>
2025-10-26 16:47:30 +0530

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 123456;
+let email="shivam@gmail.com"
+var password="shivam123"
+accountcity="pune"
+//accountId = 654321;   //not allowed
+email="dwdwegeg@df"
+password="ewrwerew"
+accountcity="mumbai"
+let accountState;
+
+console.log(accountId);
+/*prefer not to use var
+because of issue in block scope and function scope
+*/
+
+
+console.table([email,password,accountcity,accountState]);
