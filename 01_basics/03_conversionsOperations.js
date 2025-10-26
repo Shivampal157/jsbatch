32a9b14bd88df529149fa54e8644636a40868a9c
shivampal157 <shivampal60076@gmail.com>
2025-10-26 17:51:57 +0530

    conversionOperations

---
+++ 01_basics/03_conversionsOperations.js
@@ -0,0 +1,63 @@
+let score=true
+//console.log(typeof score);
+//console.log(typeof(score))
+
+let valueInNumber=Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+
+//"33"=>33
+//"
+// "33abc"=>NaN
+//true=>1
+//false=>0
+
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//1=>true
+//0=>false
+//""=>false
+//"abc"=>true
+//null=>false
+//undefined=>false
+//NaN=>false
+
+
+let someNumber="3434"
+let stringNumber=String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+//********************************Operations *********************** */
+let value=3;
+let negValue=-value
+//console.log(negValue)
+// console.log(2+2)
+// console.log(2+"2") // "22"
+// console.log(2-"2") // 0
+// console.log("2"* "2") //4
+// console.log("2"/"2") //1
+// console.log("1"+2+2) //122
+// console.log(2+2+"1") //41
+
+let str1 ="hlw"
+let str2="Shivam"
+let str3=str1 + str2
+//console.log(str3)
+// console.log(+true) //1
+// console.log(+"") //0
+
+
+let num1,num2,num3
+num1=num2=num3=2+2
+
+
+let gameCounter=100
+++
+gameCounter;
+console.log(gameCounter)
+
+//link to study https://262.ecma-international.org/#sec-abstract-operations
